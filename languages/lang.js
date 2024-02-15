c8ab9461fb83af553251edb433975bc6d6952881
reddallya <piaqasalas@gmail.com>
2024-02-15 17:01:17 -0300

    primer confirmación

---
+++ languages/lang.js
@@ -0,0 +1,22 @@
+// Day
+var word_day_sun = 'Domingo';
+var word_day_mon = 'Lunes';
+var word_day_tue = 'Martes';
+var word_day_wed = 'Miercoles';
+var word_day_thu = 'Jueves';
+var word_day_fri = 'Viernes';
+var word_day_sat = 'Sabado';
+
+// Month
+var word_month_1 = 'Enero';
+var word_month_2 = 'Febrero';
+var word_month_3 = 'Marzo';
+var word_month_4 = 'Abril';
+var word_month_5 = 'Mayo';
+var word_month_6 = 'Junio';
+var word_month_7 = 'Julio';
+var word_month_8 = 'Agosto';
+var word_month_9 = 'Septiembre';
+var word_month_10 = 'Octubre';
+var word_month_11 = 'Noviembre';
+var word_month_12 = 'Diciembre';
